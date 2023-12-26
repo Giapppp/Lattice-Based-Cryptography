cdf6acb95c6aaa684defb3e5b418971b738e0e0b
Giáp Vũ <116430098+Giapppp@users.noreply.github.com>
2023-12-26 08:46:38 +0700

    Update solve.py

--- implementation-and-testing/lattice-reduction/solve.py
+++ implementation-and-testing/lattice-reduction/solve.py
@@ -1,63 +1,87 @@
-#Source: https://hackmd.io/@hakatashi/B1OM7HFVI
-
-#Just lattice part! 
+from pwn import *
+import json
+from tqdm import tqdm
 from sage.all import *
 from sage.modules.free_module_integer import IntegerLattice
-from random import randint
-import sys
-from itertools import starmap
-from operator import mul
-from pwn import *
-import re
+import ast
 
-# Babai's Nearest Plane algorithm
-# from: http://mslc.ctf.su/wp/plaidctf-2016-sexec-crypto-300/
-def Babai_closest_vector(M, G, target):
-  small = target
-  for _ in range(1):
-    for i in reversed(range(M.nrows())):
-      c = ((small * G[i]) / (G[i] * G[i])).round()
-      small -= M[i] * c
-  return target - small
+# dimension
+n = 64
+# plaintext modulus
+p = 257
+# ciphertext modulus
+q = 1048583
 
-m = 100
-n = 12
-q = 1046961993706256953070441
-
+flen = 32
 target = process(["python3", "example.py"])
 target.recvline()
-A_values = []
-b_values = []
 
-A_pattern = re.compile(r'\[.*?\]')
-b_pattern = re.compile(r'\d+ ')
-for _ in range(100):
-  recv = target.recvline().decode()
-  A_value = eval(A_pattern.search(recv).group())
-  b_value = int(b_pattern.search(recv).group())
-  A_values.append(A_value)
-  b_values.append(b_value)
-  target.sendlineafter(b"Would you like to create another potion? (y/n):", b"y")
+def oracle_1(idx:int):
+    payload = json.dumps({"option":"get_flag","index":idx})
+    target.sendline(payload.encode())
+    data = ast.literal_eval(target.recvline().decode())
+    return data
 
-print(len(A_values))
+def oracle_2(message:int):
+    payload = json.dumps({"option":"encrypt","message":message})
+    target.sendline(payload.encode())
+    data = ast.literal_eval(target.recvline().decode())
+    return data
 
-A = matrix(ZZ, m + n, m)
-for i in range(m):
+def Babai_closest_vector(M, G, target):
+    small = target
+    for _ in range(1):
+        for i in reversed(range(M.nrows())):
+            c = ((small * G[i]) / (G[i] * G[i])).round()
+            small -= M[i] * c
+    return target - small
+
+flag_As = []
+flag_bs = []
+for idx in tqdm(range(flen)):
+    data = oracle_1(int(idx))
+    flag_As += [json.loads(data["A"])]
+    flag_bs += [int(data["b"])]
+
+As = []
+bs = []
+sample = 90
+for _ in tqdm(range(sample)):
+    data = oracle_2(int(0))
+    As += [json.loads(data["A"])]
+    bs += [int(data["b"])]
+
+p_ = pow(p, -1, q)
+bs = [(b*p_)%q for b in bs]
+
+A = Matrix(ZZ, n + sample, sample)
+for i in range(sample):
   A[i, i] = q
-for x in range(m):
+for x in range(sample):
   for y in range(n):
-    A[m + y, x] = A_values[x][y]
+    A[sample + y, x] = (As[x][y] * p_) % q
 lattice = IntegerLattice(A, lll_reduce=True)
 print("LLL done")
 gram = lattice.reduced_basis.gram_schmidt()[0]
-target = vector(ZZ, b_values)
+target = vector(ZZ, bs)
 res = Babai_closest_vector(lattice.reduced_basis, gram, target)
 print("Closest Vector: {}".format(res))
 
+for x in range(sample):
+  for y in range(n):
+    As[x][y] = (As[x][y] * p_) % q
+
 R = IntegerModRing(q)
-M = Matrix(R, A_values)
-ingredients = M.solve_right(res)
+M = Matrix(R, As)
+S = M.solve_right(res)
 
-print("Ingredients: {}".format(ingredients))
+K = GF(q)
+flag = []
+for idx in range(flen):
+    A_ = vector(K, flag_As[idx])
+    b_ = K(flag_bs[idx])
+    x = int(b_ - S * A_) % q
+    x = x if x < q//2 else x - q
+    flag += [x%p]
 
-
+print(bytes(flag))
